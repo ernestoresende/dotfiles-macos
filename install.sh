a196f5c80128ba2ecf35eeb991ad2538dfdd91e1
Ernesto Resende <ernestoresende@hotmail.com>
2020-06-15 23:21:55 -0300

    removed: zsh automation and opening download links

--- install.sh
+++ install.sh
@@ -28,33 +28,16 @@ eval "sh scripts/brew.sh"
 printf "\n${BLUE}Installing Oh My Zsh...${NORMAL}\n"
 eval "sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'"
 
-# Copy ZSH config
-printf "${YELLOW}Copying ZSH config into ~/.zshrc...${NORMAL}\n"
-eval "cp ./zshrc ~/.zshrc"
-
 # Install fonts
 eval "sh scripts/fonts.sh"
 
 # Set up VS Code Settings and Extensions
 eval "sh scripts/vscode.sh"
 
-# Add global NPM packages
-printf "\n${GREEN}Installing global npm packages for GatsbyCLI${NORMAL}\n"
-eval "npm install -g gatsby-cli"
-
 # Refresh ZSH config
 eval "source ~/.zshrc"
 
-# Open links for apps that can't be downloaded via brew cask
-printf "\n${BLUE}Opening links for apps to download...${NORMAL}\n"
-
-# =========================
-# Put here all the programs you cannot download with "brew cask install"
-
-# Productivity
-eval "open https://www.google.com/intl/pt-BR_ALL/drive/download/backup-and-sync/"
-
 printf "\n${WHITE}==============================================================================
 ${GREEN}All set!
-${WHITE}Make sure to configure yout Git and sync it with Github.
+${WHITE}Make sure to configure your Git and sync it with Github.
 ==============================================================================${NORMAL}\n"
